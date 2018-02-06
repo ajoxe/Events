9a1a1819dff486cb59233c070a5462f63b0f5f5d
Amirah Oxendine <amirahoxendine@ac.c4q.nyc>
2018-02-05 21:39:11 -0500

    added packages and empty classes for database and jobs

---
+++ app/src/main/java/com/example/android/events/database/EventsDataBaseHelper.java
@@ -0,0 +1,8 @@
+package com.example.android.events.database;
+
+/**
+ * Created by amirahoxendine on 2/5/18.
+ */
+
+public class EventsDataBaseHelper {
+}
---
+++ app/src/main/java/com/example/android/events/jobs/DatabaseJob.java
@@ -0,0 +1,8 @@
+package com.example.android.events.jobs;
+
+/**
+ * Created by amirahoxendine on 2/5/18.
+ */
+
+public class DatabaseJob {
+}
