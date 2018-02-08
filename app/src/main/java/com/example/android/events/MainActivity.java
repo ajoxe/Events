ef6d30cdf4c048d0ff26b7c161100b3b19ff077b
Amirah Oxendine <amirahoxendine@ac.c4q.nyc>
2018-02-07 21:43:36 -0500

    merge

--- app/src/main/java/com/example/android/events/MainActivity.java
+++ app/src/main/java/com/example/android/events/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.events;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //EventsDatabase db = Room.databaseBuilder(getApplicationContext(),
+                //EventsDatabase.class, "events").build();
     }
+
+    public void nextClass(View view) {
+        Intent i = new Intent(MainActivity.this, EventsList.class);
+        startActivity(i);
+
+    }
+
 }
