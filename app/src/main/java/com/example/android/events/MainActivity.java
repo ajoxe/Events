c3157320b55994f4d8cd78f7f2c450208efac6f3
Melina Gonzalez <melg@Melinas-MacBook-Pro.local>
2018-02-10 00:27:39 -0500

    UI changes

--- app/src/main/java/com/example/android/events/MainActivity.java
+++ app/src/main/java/com/example/android/events/MainActivity.java
@@ -4,16 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.android.events.roomdatabase.DatabaseInitializer;
 import com.example.android.events.roomdatabase.EventsDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView logoImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logoImage = findViewById(R.id.logo_image);
+
         DatabaseInitializer.populateAsync(EventsDatabase.getEventsDatabase(this));
         DatabaseInitializer.queryDb(EventsDatabase.getEventsDatabase(this));
 
@@ -32,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
-        EventsDatabase.destroyInstance();
+//        EventsDatabase.destroyInstance();
         super.onDestroy();
     }
 
